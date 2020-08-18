8915b64abf2154afc7899fb8b18f71874aa72a5c
Marques Avery <averymarques4@gmail.com>
2020-08-18 16:52:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,13 +33,12 @@ class Song
 
   def self.genre_count
     genre_list = Hash.new
-    @@genres.each do |item|
+    @@genres.collect do |item|
       if genre_list.include?(item)
         genre_list[item] += 1
       else
         genre_list[item] = 1
       end
-      genre_list
     end
   end
 
